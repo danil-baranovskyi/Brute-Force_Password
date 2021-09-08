d7a2df31cbeebd9aca8e852783288ee3560dc8f6
Danil Baranovskyi <danbaranovsyi@gmail.com>
2021-09-08 22:24:26 +0300

    first commit

--- index.js
+++ index.js
@@ -1,83 +1,86 @@
-//1) Функция логин, создать массив символов.
-//2) Создать функцию матрицы, ключ и значение. Где ключ это текущий символ в слове а значение это его соответсвие из массива разрешённых символов.
-//3) Функция текущего инкреметируемого значения. Определяет какой ключ сейчас перебираеться.
-//4) При проходе значения у ключа до конца массива разрешённых символов, обнулить текущее значение и перейти выше.
-//5) Функция инкремента принимает 3 аргумента матрицу(объект), преведущее значение и текущеее итерируемое.
-//6) Функция преобразования текущей матрицы в строку.
-//7) Проверка строки(Пароля в логине) в цикле.
-
-
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const letters = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // functions
 function brute(maxLength) {
+    let resolve
+    let a;
+    for (let matrixLength = 0; matrixLength < maxLength; matrixLength++) {
+        a = matrix(matrixLength);
+        let index;
+        while ((index = getIndex(a, letters.length)) !== null) {
+            resolve = increment(a, index);
+            if (login(matrixToString(resolve))) {
 
-
-  for (let matrixLength = 1; matrixLength <= maxLength; matrixLength++) {
-    let a = matrix(matrixLength);
-
-  }
+                console.log("You login is : " + matrixToString(resolve))
+                return true;
+            }
+        }
+    }
 }
 
 
 function login(pass) { //LOGIN
-  return pass === "aa";
+    return pass === "zzzz";
 }
 
 
 function matrix(length) {
-  let matrix = {};
+    let matrix = {};
 
-  for (let i = 0; i <= length; i++) {
-    matrix[i] = 0;
-  }
+    for (let i = 0; i <= length; i++) {
+        matrix[i] = 0;
+    }
 
-  return matrix;
+    return matrix;
 }
 
 
 function getIndex(matrix, arrLength) {
 
-  for (let i = Object.values(matrix).length - 1; i >= 0; i--) {
-    if (matrix[i] < arrLength - 1) {
-      return i;
+    for (let i = Object.values(matrix).length - 1; i >= 0; i--) {
+        if (matrix[i] < arrLength - 1) {
+            return i;
+        }
     }
-  }
 
-  return null;
+    return null;
 }
 
 function increment(matrix, index, prevIndex = null) {
-  matrix[index]++;
 
-  if (prevIndex === null || index === prevIndex) {
+    matrix[index]++;
+
+    // if (prevIndex === null || index === prevIndex) {
+    //   return matrix;
+    // }
+
+    for (let i = index + 1; i < Object.values(matrix).length; i++) {
+        matrix[i] = 0;
+    }
+
     return matrix;
-  }
-
-  for (let i = index + 1; i < Object.values(matrix).length; i++) {
-    matrix[i] = 0;
-  }
-
-  return matrix;
 }
 
 function matrixToString(matrix) { //преобразование матрицы в строку
-  const valuesArr = Object.values(matrix);
-  let str = '';
+    const valuesArr = Object.values(matrix);
+    let str = '';
 
-  valuesArr.forEach(el => {
-    str += letters[el];
-  })
+    valuesArr.forEach(el => {
+        str += letters[el];
+    })
 
-  return str;
+    return str;
 }
 
 
 ///
-brute(3);
-// console.log(getIndex({
-//   0: 43,
-//   1: 42,
-//   2: 43
-// },letters.length))
+brute(4);
+
+
+// let some = {
+//   0 : 1,
+//   1 : 51
+// }
+// let index;
+// console.log(increment(some, getIndex(some, letters.length)))
